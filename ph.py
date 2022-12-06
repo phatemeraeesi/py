d2d748d525b542dafb8bd0b5dc12822e9be328a4
phatemeraeesi <119856729+phatemeraeesi@users.noreply.github.com>
2022-12-07 00:32:10 +0330

    Add files via upload

---
+++ ph.py
@@ -0,0 +1,9 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from collections import Counter
+... num_k =str(input(""))
+... num_h = str(input(""))
+... z=Counter(num_k)+Counter(num_h)
+... print(z)
+... x=len(num_k)+len(num_h)
+... print(x) 
